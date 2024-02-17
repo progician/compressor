8b6bbce273b54cf6dcfc9e929e6f2520a22e7871
Gyula Gubacsi <gyula.gubacsi@gmail.com>
2024-02-17 19:02:39 +0000

    refactor(test): Use a common fixture for test cases
    There's code duplication between the two tests cases, which leads to
    less clear tests. In fact, the differences are minimal.
    
    But even more importantly, removing duplication also makes the tests more
    expressive and easier to understand. We would be testing the code on the
    level it's running on.

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,8 +1,8 @@
 import pytest
 
-from compressor import db
 from bs4 import BeautifulSoup, Tag
-from compressor import create_app, shortened_urls
+from compressor import db, create_app, shortened_urls
+from dataclasses import dataclass
 from flask import Flask
 from flask.testing import FlaskClient
 from pathlib import Path
@@ -25,40 +25,46 @@ def client(app: Flask) -> FlaskClient:
     return client
 
 
-def test_shorten_and_redirect_to_url(client: FlaskClient):
+@dataclass
+class CompressorApp:
+    app: Flask
+    client: FlaskClient
+
+    def shorten(self, url: str) -> str:
+        response = self.client.post("/", data={"url": url})
+        assert response.status_code == 200
+
+        page = BeautifulSoup(response.data.decode("utf-8"), "html.parser")
+        short_url_anchor = page.find("a", id="short-url")
+        assert isinstance(short_url_anchor, Tag)
+
+        shorten_href = short_url_anchor.attrs["href"]
+        assert shorten_href is not None
+        return shorten_href
+
+    def redirect(self, short_url: str) -> str:
+        response = self.client.get(short_url)
+        assert response.status_code == 302
+        return response.headers["Location"]
+
+
+@pytest.fixture
+def compressor_app(app: Flask, client: FlaskClient) -> CompressorApp:
+    return CompressorApp(app, client)
+
+
+def test_shorten_and_redirect_to_url(compressor_app: CompressorApp) -> None:
     url = "https://www.google.com"
-    response = client.post("/", data={"url": url})
-    assert response.status_code == 200
-
-    page = BeautifulSoup(response.data.decode("utf-8"), "html.parser")
-    short_url_anchor = page.find("a", id="short-url")
-    assert isinstance(short_url_anchor, Tag)
-
-    short_url = short_url_anchor.attrs["href"]
-
-    assert short_url is not None and short_url != url and url not in short_url
-
-    response = client.get(short_url)
-    assert response.status_code == 302
-    assert response.headers["Location"] == url
+    short_url = compressor_app.shorten(url)
+    assert url not in short_url
+    assert compressor_app.redirect(short_url) == url
 
 
-def test_url_shortening_is_persistent(client: FlaskClient):
+def test_url_shortening_is_persistent(compressor_app: CompressorApp) -> None:
     url = "https://www.google.com"
-    response = client.post("/", data={"url": url})
-    assert response.status_code == 200
+    short_url = compressor_app.shorten(url)
 
     global shortened_urls
     shortened_urls.clear()
 
-    page = BeautifulSoup(response.data.decode("utf-8"), "html.parser")
-    short_url_anchor = page.find("a", id="short-url")
-    assert isinstance(short_url_anchor, Tag)
-
-    short_url = short_url_anchor.attrs["href"]
-
-    assert short_url is not None and short_url != url and url not in short_url
-
-    response = client.get(short_url)
-    assert response.status_code == 302
-    assert response.headers["Location"] == url
+    assert compressor_app.redirect(short_url) == url
